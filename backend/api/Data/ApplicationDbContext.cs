ea7622543b3a5cfa66143d0659707d06b964a701
Valentyn Saik <svbackend22@gmail.com>
2025-10-04 12:03:52 +0200

    copy skeleton of the backend app

---
+++ backend/api/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+using api.Modules.User.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<ApiToken> ApiTokens { get; set; }
+    public DbSet<OneTimePassword> OneTimePasswords { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.ApplyConfiguration(new OneTimePassword.Configuration());
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder.UseSnakeCaseNamingConvention());
+    }
+}
